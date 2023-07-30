78edb60e4a48316088f35832c78dbc56c5efee3b
SAURABH SATAPATHY <136247426+saurabh-IITM@users.noreply.github.com>
2023-07-30 14:25:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Python Program to check if Candidate is Eligible for Driving Licence.
+Age = int(input("Enter the Age :--> "))
+if Age > 18:
+print("The person is eligible for driving license ")
+else:
+print("The person is not eligible for driving license ")
